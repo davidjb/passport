2115ce1f3e39ccde478277f30f424cc2702203e9
David Beitey <david@davidjb.com>
2016-08-08 01:00:23 +1000

    Symlink index for android, until we need otherwise

--- index.android.js
+++ index.android.js
@@ -1,15 +1,1 @@
-import React from 'react'
-import { AppRegistry } from 'react-native'
-import Root from './App/Root'
-import './App/Config/ReactotronConfig'
-import configureStore from './App/Store/Store'
-
-// Handling store here to avoid hot-reloading issues
-const store = configureStore()
-class RNBase extends React.Component {
-  render () {
-    return <Root {...this.props} store={store} />
-  }
-}
-
-AppRegistry.registerComponent('PassportApp', () => RNBase)
+index.ios.js
